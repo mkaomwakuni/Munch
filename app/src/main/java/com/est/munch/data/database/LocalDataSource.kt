440efe6d9b5807423faefc47522cd61f1a5ee486
mkaocodes <mkaoibrahim@gmail.com>
2024-11-25 02:29:12 +0300

    feat: Add Local and Remote Data Sources classes to handle data operations.

---
+++ app/src/main/java/com/est/munch/data/database/LocalDataSource.kt
@@ -0,0 +1,45 @@
+package com.est.munch.data.database
+
+import com.est.munch.data.database.local.dao.MunchDao
+import com.est.munch.data.database.local.entities.BookedRecipeEntity
+import com.est.munch.data.database.local.entities.FoodJokeEntity
+import com.est.munch.data.database.local.entities.RecipeEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class LocalDataSource @Inject constructor(
+    private val munchDao: MunchDao
+){
+
+    fun readRecipes(): Flow<List<RecipeEntity>> {
+        return munchDao.readRecipes()
+    }
+
+    fun readBooked() : Flow<List<BookedRecipeEntity>> {
+        return munchDao.readBookedRecipes()
+    }
+
+    fun readJokes() : Flow<List<FoodJokeEntity>> {
+        return  munchDao.readFoodJoke()
+    }
+
+    suspend fun insertRecipes(recipeEntity: RecipeEntity) {
+        munchDao.insertRecipes(recipeEntity)
+    }
+
+    suspend fun insertBooked(bookedRecipeEntity: BookedRecipeEntity) {
+        munchDao.insertBookedMarked(bookedRecipeEntity)
+    }
+
+    suspend fun insertJokes(foodJokeEntity: FoodJokeEntity) {
+        munchDao.insertFoodJoke(foodJokeEntity)
+    }
+
+    suspend fun deleteBookedRecipe(bookedRecipeEntity: BookedRecipeEntity) {
+        munchDao.deleteBookedRecipe(bookedRecipeEntity)
+    }
+
+    suspend fun deleteAllBooked() {
+        munchDao.deleteAllBookedRecipe()
+    }
+}
