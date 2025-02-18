f85b49dcef208c21b63aaa4df6d63da69aabe43b
mkaocodes <mkaoibrahim@gmail.com>
2025-02-18 22:21:01 +0300

    chore: removed secrets

--- app/src/main/java/com/est/munchy/utils/Constants.kt
+++ app/src/main/java/com/est/munchy/utils/Constants.kt
@@ -29,7 +29,6 @@ class AppConstants {
 
         const val BASE_URL = "https://api.spoonacular.com"
         const val BASE_IMAGE_URL = "https://spoonacular.com/cdn/ingredients_100x100/"
-        const val API_KEY = ""
 
         const val RECIPE_RESULT_KEY = "recipeBundle"
 
